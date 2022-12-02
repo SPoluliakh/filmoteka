fb74214e7ead567e16ec3629b35d0b70c370d9e5
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-02 23:53:02 +0200

    updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import * as SC from './Footer.styled';
 import SV from '../../Utils/Sergii_Poluliakh_frontend developer.pdf';
+import { Box } from 'Utils/Box';
 
 export const Footer = () => {
   return (
@@ -7,10 +8,21 @@ export const Footer = () => {
       <SC.Footer>
         <SC.FooterContainer>
           <SC.FooterInnerWrap>
-            <SC.FooterText>© 2022 | All Rights Reserved |</SC.FooterText>
-            <SC.FooterLink href={SV} target="_blank">
-              Developers info
-            </SC.FooterLink>
+            <Box display="flex" flexDirection="column" alignItems="flex-start">
+              <SC.FooterTextCertificateInfo>
+                TMDB logo to identify your use of the TMDB APIs.
+              </SC.FooterTextCertificateInfo>
+              <SC.FooterTextCertificateInfo>
+                "This product uses the TMDB API but is not endorsed or certified
+                by TMDB."
+              </SC.FooterTextCertificateInfo>
+            </Box>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <SC.FooterText>© 2022 | All Rights Reserved |</SC.FooterText>
+              <SC.FooterLink href={SV} target="_blank">
+                About developer
+              </SC.FooterLink>
+            </Box>
           </SC.FooterInnerWrap>
         </SC.FooterContainer>
       </SC.Footer>
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-  border-top: 2px solid navajowhite;
+  border-top: 0.8px solid navajowhite;
   width: 100vw;
 `;
 
@@ -12,7 +12,7 @@ export const FooterContainer = styled.div`
 
 export const FooterInnerWrap = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
@@ -30,17 +30,27 @@ export const FooterText = styled.p`
 `;
 
 export const FooterLink = styled.a`
-  font-family: inherit;
   font-size: 16px;
   line-height: 1.17;
   font-style: normal;
   font-weight: 400;
   cursor: pointer;
-
   color: navajowhite;
   text-decoration: underline;
+  transition: scale 250ms linear;
 
   :hover {
-    scale: 1.2;
+    scale: 1.1;
   }
+`;
+
+export const FooterTextCertificateInfo = styled.p`
+  color: navajowhite;
+  font-size: 10px;
+  line-height: 1.17;
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+  margin-top: 0;
+  margin-bottom: 0;
 `;
